d7fbcf9f999d09d1af6c49949e81ca670ed9b1fc
Coronilla <e.coronillahernandez@ugto.com>
2024-10-30 22:34:50 -0600

    Configuración de Firebase

---
+++ SmartTrainer/config/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: proccess.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "smarttrainer-e3e9a.firebaseapp.com",
+  projectId: "smarttrainer-e3e9a",
+  storageBucket: "smarttrainer-e3e9a.appspot.com",
+  messagingSenderId: "77716023729",
+  appId: "1:77716023729:web:5ef229fd948b9fb390f590"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
